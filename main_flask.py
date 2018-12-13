d95636b7202fd75019340e48d09369a86042acf9
Calvin <cas542@cornell.edu>
2018-12-13 03:16:36 -0500

    Front

--- main_flask.py
+++ main_flask.py
@@ -1,16 +1,20 @@
+#to run:
+#      main_flask.py run 
 # from main import classify //having errors with importing other libraries from main
+import funFactScraper 
 from flask import Flask
 app = Flask (__name__)
 
 
 @app.route("/")
 def index():
-    return "Welcome"
+    return "Welcome" #test
 
 @app.route("/processPic/<pic>")
 def processPic(pic):
     # result = classify(pic)
-    return "done" #should be return result 
+    # scrape using result
+    return "done" #should be return json with fields result and fun 
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// to run :
+//     1. npm install
+//     2. npm start
 import React, { Component } from 'react';
 import axios from "axios";
 import Cropper from "react-cropper";
@@ -10,7 +13,7 @@ class App extends Component {
   state={
     imageUploaded: false,
     imageProcessing: false,
-    imageProcessingResult: "Kendrick Lamar",
+    inspectionResult: "Kendrick Lamar",
     selectedImage: null,
     funFact: "I am the one"
   }
@@ -27,12 +30,12 @@ class App extends Component {
     )
 }
 
+  //places a get request that should return an object containing result of neural net and fun fact
   processPicture = async (e) => {   
-    // e.preventDefault()       
    if (this.state.imageProcessing) { 
     await axios.get(`http://127.0.0.1:5000/processPic/${this.state.selectedImage}`)
    .then((res) => {
-              this.setState({imageProcessingResult: res}, function (){console.log("yay")})})
+              this.setState({inspectionResult: res}, function (){console.log("yay")})})
     .catch((e)=> {
       console.log("Oops")
     })           
@@ -51,7 +54,7 @@ class App extends Component {
                 <div className="col-xs-7 form-container">
                   <Form getPicture={this.getPicture} processPicture={this.processPicture} />
 
-                  <ShowResults result={this.state.imageProcessingResult} funFact={this.state.funFact}
+                  <ShowResults result={this.state.inspectionResult} funFact={this.state.funFact}
                    image={this.state.selectedImage}/>
                 </div>
               </div>
