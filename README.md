a299279c390b75c256a0f7eec915dabfdadea0c0
Calvin <cas542@cornell.edu>
2018-12-15 07:59:47 -0500

    First deploy

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # funfactsanimals
 CS 4701 Project
+Execute as follows:
+1. run two cmd shells
+2. a. npm install
+   b. npm start (Launches ReactDOM)
+3. py main_flask.py run (Launches Flask server)
--- main_flask.py
+++ main_flask.py
@@ -1,14 +1,19 @@
-#to run:
-#      main_flask.py run 
+# Launches server on port 3001
+# Command: main_flask.py run 
 from main import classify
 from flask import Flask, request, jsonify
 app = Flask (__name__)
 
-
+# Trivial route/"Home"
 @app.route("/")
 def index():
-    return jsonify ({"result":"Welcome"}) #test
+    return jsonify ({"result":"Welcome"}) #trivial
 
+# Returns JSON object containing a field for the result of the neural net's prediction
+# and a field for a fun fact. It achieves this by the following procedure:
+# 1. Extracts base64 string representation of the uploaded image from the request object,
+# 2. Removes metadata tags
+# 3. invokes classify function from main on the resulting string
 @app.route("/processPic/", methods=['GET','POST'])
 def processPic():
     picbase64 = request.get_json()["picbase64"]
